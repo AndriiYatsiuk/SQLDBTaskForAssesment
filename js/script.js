5a304ada0a63c4c0ad26de9c58b40c14d8de0768
Andrii Yatsiuk <thenmymail@gmail.com>
2023-05-28 23:08:19 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ $(document).ready(function () {
 		$('body').toggleClass('lock')
 	});
 });
+
+//zoom back img by click. Get all the images with the "zoomable" class
+const images = document.querySelectorAll('.clients-adv-content img, .nextadventure-mainpage img');
+
+// Attach a click event listener to each image
+images.forEach(image => {
+	image.addEventListener('click', function () {
+		this.classList.toggle('zoomed');
+	});
+});
